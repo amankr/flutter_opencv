6c72a4d3bd2bfe998ab0a9ac4eb22953d3a047dc
Aman Kumar <amank0924@gmail.com>
2020-07-02 01:38:11 +0530

    perspective transformation done

--- android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
+++ android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
@@ -13,6 +13,7 @@ import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfInt;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -652,13 +653,6 @@ public class CVCore {
         return byteArray;
     }
 
-    String ds(float[] p){
-        StringBuilder sb = new StringBuilder();
-        for (int i=0;i<p.length;++i)
-            sb.append(p[i]+", ");
-        return sb.toString();
-    }
-
     @SuppressLint("MissingPermission")
     public byte[] perspectiveTransformation(byte[] byteData, ArrayList points){
         byte[] byteArray = new byte[0];
@@ -666,16 +660,17 @@ public class CVCore {
         try{
             if(points.size() != pointSize)
                 throw new Exception("points should be 8");
-            Mat rect = new Mat(4, 2, CvType.CV_32F);
-            float[] rp = new float[pointSize];
-            for (int i=0;i<pointSize;++i)
-                rp[i] = (float)(double)points.get(i);
-            rect.put(4, 2, rp);
+
             System.out.println("OpenCV Rect Matrix");
-            Point tl = new Point(rp[0], rp[1]);
-            Point tr = new Point(rp[2], rp[3]);
-            Point br = new Point(rp[4], rp[5]);
-            Point bl = new Point(rp[6], rp[7]);
+            Point tl = new Point((double)points.get(0), (double)points.get(1));
+            Point tr = new Point((double)points.get(2), (double)points.get(3));
+            Point br = new Point((double)points.get(4), (double)points.get(5));
+            Point bl =new Point((double)points.get(6), (double)points.get(7));
+            List<Point> rectPoints = new ArrayList<Point>();
+            rectPoints.add(tl);
+            rectPoints.add(tr);
+            rectPoints.add(br);
+            rectPoints.add(bl);
 
             double widthA = Math.sqrt(((br.x-bl.x) * (br.x-bl.x)) + ((br.y - bl.y) * (br.y - bl.y)));
             double widthB = Math.sqrt(((tr.x-tl.x) * (tr.x-tl.x)) + ((tr.y - tl.y) * (tr.y - tl.y)));
@@ -685,23 +680,28 @@ public class CVCore {
             double heightB = Math.sqrt(((tl.x-bl.x) * (tl.x-bl.x)) + ((tl.y-bl.y) * (tl.y-bl.y)));
             int maxHeight = Math.max((int)heightA, (int)heightB);
 
-            Mat dst = new Mat(4, 2, CvType.CV_32F);
-            float[] dstData = {0, 0, maxWidth-1, 0, maxWidth-1, maxHeight-1, 0, maxHeight-1};
-            dst.put(4, 2, dstData);
+            List<Point> dstPoints = new ArrayList<Point>();
+            dstPoints.add(new Point(0, 0));
+            dstPoints.add(new Point(maxWidth-1, 0));
+            dstPoints.add(new Point(maxWidth-1, maxHeight-1));
+            dstPoints.add(new Point(0, maxHeight-1));
 
-            System.out.println("OpenCV Rect: " + ds(rp));
-            System.out.println("OpenCV DST: " + ds(dstData));
+            Mat rect = Converters.vector_Point2f_to_Mat(rectPoints);
+            Mat dst = Converters.vector_Point2f_to_Mat(dstPoints);
 
             Mat M = Imgproc.getPerspectiveTransform(rect, dst);
-            System.out.println("OpenCV Perspective Matrix ");
+            System.out.println("OpenCV Perspective Matrix "+M.rows()+" "+M.cols()+" "+M);
+
             Mat wrapped = new Mat();
             Mat src = Imgcodecs.imdecode(new MatOfByte(byteData), Imgcodecs.IMREAD_UNCHANGED);
-            Imgproc.warpPerspective(src, wrapped, M, new Size(maxWidth, maxHeight));
+            Mat rotate = new Mat();
+            Core.rotate(src, rotate, Core.ROTATE_90_CLOCKWISE);
+            Imgproc.warpPerspective(rotate, wrapped, M, new Size(maxWidth, maxHeight));
             System.out.println("OpenCV warpPerspective ");
             MatOfByte matOfByte = new MatOfByte();
             Imgcodecs.imencode(".jpg", wrapped, matOfByte);
             byteArray = matOfByte.toArray();
-            System.out.println("OpenCV Result "+byteArray.length);
+            System.out.println("OpenCV Result "+byteArray.length + " "+wrapped.rows()+ " "+wrapped.cols());
         }catch (Exception e){
             System.out.println("OpenCV Error: " + e.toString());
         }
